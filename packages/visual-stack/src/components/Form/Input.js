f27c1db54c75b536b3e9562523b09182fad55f4a
Nicolás Font <nicolasfont@gmail.com>
2020-06-01 16:53:13 -0700

    Add trimmed prop to Input

--- packages/visual-stack/src/components/Form/Input.js
+++ packages/visual-stack/src/components/Form/Input.js
@@ -1,17 +1,35 @@
+import * as R from 'ramda';
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Form.css';
 
-const Input = ({ className, type, ...restProps }) => (
+const Input = ({
+  className,
+  onBlur,
+  onChange,
+  trimmed,
+  type,
+  ...restProps
+}) => (
   <input
     {...restProps}
     type={type || 'text'}
     className={`form-control ${className || ''}`}
+    onChange={onChange}
+    onBlur={e => {
+      if (trimmed && onChange) {
+        onChange(R.over(R.lensPath(['target', 'value']), R.trim)(e));
+      }
+      if (onBlur) {
+        onBlur();
+      }
+    }}
   />
 );
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
+  trimmed: PropTypes.bool,
 };
 
 export default Input;
