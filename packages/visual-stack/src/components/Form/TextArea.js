0ba04fd059656dd9cd99033ff74eae1fec9eebfe
Nicolás Font <nicolasfont@gmail.com>
2020-06-02 15:46:23 -0700

    Add trimmed prop to TextArea

--- packages/visual-stack/src/components/Form/TextArea.js
+++ packages/visual-stack/src/components/Form/TextArea.js
@@ -1,6 +1,24 @@
+import * as R from 'ramda';
 import React from 'react';
 import './Form.css';
 
-export default ({ className, ...restProps }) => (
-  <textarea {...restProps} className={`form-control ${className || ''}`} />
+export default ({ className, onBlur, onChange, trimmed, ...restProps }) => (
+  <textarea
+    {...restProps}
+    className={`form-control ${className || ''}`}
+    onChange={onChange}
+    onBlur={e => {
+      const maybeTrimmedEvent = trimmed
+        ? R.over(R.lensPath(['target', 'value']), R.trim)(e)
+        : e;
+
+      if (onChange) {
+        onChange(maybeTrimmedEvent);
+      }
+
+      if (onBlur) {
+        onBlur(maybeTrimmedEvent);
+      }
+    }}
+  />
 );
