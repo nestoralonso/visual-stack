a653ba4c1ddcff0f8a153529c627ec10d8cdb987
Josh Nixon <jnixon@cj.com>
2020-10-15 13:27:38 -0700

    removed state from tool tip

--- packages/visual-stack/src/components/ToolTip/index.js
+++ packages/visual-stack/src/components/ToolTip/index.js
@@ -3,82 +3,67 @@ import './styles.css';
 import InformationOutlineIcon from 'mdi-react/InformationOutlineIcon';
 import React, { Component, createRef } from 'react';
 
-class ToolTipConstructor extends Component {
-  constructor(props) {
-    super(props);
-    const {
-      offsetGap,
-      offset,
-      width,
-      direction,
-      children,
-      className,
-      ...rest
-    } = props;
+const ToolTipConstructor = ({
+  offsetGap,
+  offset,
+  width,
+  direction,
+  children,
+  className,
+  ...rest
+}) => {
 
-    let toolTipStyles;
+  let toolTipStyles;
 
-    switch (direction) {
-      case 'top':
-        toolTipStyles = {
-          width: width ? parseInt(width, 10) : 140,
-          marginLeft: width ? -Math.round(width / 2) : -70,
-          bottom: `${120 + (parseInt(offsetGap, 10) || 0)}%`,
-          left: `${50 + (parseInt(offset, 10) || 0)}%`,
-        };
-        break;
-      case 'left':
-        toolTipStyles = {
-          width: parseInt(width, 10),
-          right: `${115 + (parseInt(offsetGap, 10) || 0)}%`,
-          top: `${-5 + (parseInt(offset, 10) || 0)}px`,
-        };
-        break;
-      case 'right':
-        toolTipStyles = {
-          width: parseInt(width, 10),
-          left: `${120 + (parseInt(offsetGap, 10) || 0)}%`,
-          top: `${-5 + (parseInt(offset, 10) || 0)}px`,
-        };
-        break;
-      default:
-        toolTipStyles = {
-          width: width ? parseInt(width, 10) : 140,
-          marginLeft: width ? -Math.round(width / 2) : -70,
-          top: `${115 + (parseInt(offsetGap, 10) || 0)}%`,
-          left: `${50 + (parseInt(offset, 10) || 0)}%`,
-        };
-    }
+  switch (direction) {
+    case 'top':
+      toolTipStyles = {
+        width: width ? parseInt(width, 10) : 140,
+        marginLeft: width ? -Math.round(width / 2) : -70,
+        bottom: `${120 + (parseInt(offsetGap, 10) || 0)}%`,
+        left: `${50 + (parseInt(offset, 10) || 0)}%`,
+      };
+      break;
+    case 'left':
+      toolTipStyles = {
+        width: parseInt(width, 10),
+        right: `${115 + (parseInt(offsetGap, 10) || 0)}%`,
+        top: `${-5 + (parseInt(offset, 10) || 0)}px`,
+      };
+      break;
+    case 'right':
+      toolTipStyles = {
+        width: parseInt(width, 10),
+        left: `${120 + (parseInt(offsetGap, 10) || 0)}%`,
+        top: `${-5 + (parseInt(offset, 10) || 0)}px`,
+      };
+      break;
+    default:
+      toolTipStyles = {
+        width: width ? parseInt(width, 10) : 140,
+        marginLeft: width ? -Math.round(width / 2) : -70,
+        top: `${115 + (parseInt(offsetGap, 10) || 0)}%`,
+        left: `${50 + (parseInt(offset, 10) || 0)}%`,
+      };
+  }
 
-    this.state = {
-      styles: toolTipStyles,
-    };
-
-    this.tooltipClassNames = `tooltip tooltip-${direction}`;
-    this.customClassNames = className
+  const tooltipClassNames = `tooltip tooltip-${direction}`;
+  const customClassNames = className
       ? 'tooltip-hint'
       : `tooltip-hint ${className}`;
 
-    this.children = children;
-    this.rest = rest;
-    this.ref = createRef();
-  }
-
-  render() {
-    return (
-      <div className={this.customClassNames} {...this.rest}>
-        {this.children[0]}
-        <span
-          ref={this.ref}
-          className={this.tooltipClassNames}
-          style={this.state.styles}
-        >
-          {this.children[1]}
-        </span>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={customClassNames} {...rest}>
+    {children[0]}
+    <span
+      className={tooltipClassNames}
+      style={toolTipStyles}
+    >
+      {children[1]}
+    </span>
+  </div>
+  );
+};
 
 export const ToolTipWithIcon = ({ children, iconSize, ...rest }) => {
   return (
@@ -112,6 +97,8 @@ export const ToolTip = ({ children, ...rest }) => {
     </ToolTipConstructor>
   );
 };
+
+
 
 export default {
   ToolTip,
