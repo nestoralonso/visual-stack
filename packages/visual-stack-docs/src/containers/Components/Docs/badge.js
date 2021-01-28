a09f880485b56c7fe5500e47a59ad7dca9738705
Nicolás Font <nicolasfont@gmail.com>
2021-01-28 07:06:08 -0800

    Add default Badge background color

--- packages/visual-stack-docs/src/containers/Components/Docs/badge.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/badge.js
@@ -18,6 +18,21 @@ export default () => (
           </Panel>
           <Panel>
             <Body>
+              {/* s4:start */}
+              <Box direction="row" gap="default">
+                <Badge>CJ Performer</Badge>
+                <Badge>CJ Spotlight</Badge>
+                <Badge>Content Certified</Badge>
+                <Badge>New to Network</Badge>
+                <Badge>SubAffiliate</Badge>
+              </Box>
+              {/* s4:end */}
+              <Snippet tag="s4" src={snippets} />
+            </Body>
+          </Panel>
+
+          <Panel>
+            <Body>
               {/* s2:start */}
               <Box direction="row" gap="default">
                 <Badge backgroundColor="#9C52F7">CJ Performer</Badge>
