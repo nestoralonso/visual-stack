7333615d26f0d6f9a3d0916f239394c0bef1b301
Anthony Brice <anthonymbrice@gmail.com>
2021-06-16 13:20:24 -0700

    make company selector a class component

--- packages/visual-stack-docs/src/containers/Components/Docs/company-selector.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/company-selector.js
@@ -10,6 +10,18 @@ export default () => (
     {snippets => (
       <>
         <Snippet tag="s1" src={snippets} />
+        <br/>
+        <Snippet tag="s4" src={snippets} />
+        {/* s4:start */}
+        <CompanySelector
+          selectedCompany={{ name: 'Foobar', id: '111111' }}
+          companies={[
+            { name: 'Foobar', id: '111111' },
+            { name: 'Bazbox', id: '222222' },
+          ]}
+        />
+        {/* s4:end */}
+        <br/>
         <Snippet tag="s2" src={snippets} />
         {/* s2:start */}
         <CompanySelector
@@ -18,8 +30,20 @@ export default () => (
             { name: 'Foobar', id: '111111' },
             { name: 'Bazbox', id: '222222' },
           ]}
+          selectText="Look!"
+          searchAccountsText="These are the accounts."
         />
         {/* s2:end */}
+        <br/>
+        <Snippet tag="s3" src={snippets} />
+        {/* s3:start */}
+        <CompanySelector
+          selectedCompany={{ name: 'Foobar', id: '111111' }}
+          companies={[
+            { name: 'Foobar', id: '111111' }
+          ]}
+        />
+        {/* s3:end */}
       </>
     )}
   </Demo>
