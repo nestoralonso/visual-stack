50fe466a014c398ceba61fa4dc8f70ec07b05f9b
Nicolás Font <nicolasfont@gmail.com>
2021-08-25 15:36:02 -0700

    Revert "Merge pull request #363 from nestoralonso/patch-3"
    This reverts commit bb590a14857e05b4f0776962d338af79b21322c0, reversing
    changes made to aaff3487222202c6500bdeaf8dc1b49e89e5f5b6.

--- packages/visual-stack/src/components/SideNav/CategoryLabel.js
+++ packages/visual-stack/src/components/SideNav/CategoryLabel.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './SideNav.css';
 
 export const CategoryLabel = ({ children, collapsed, ...restProps }) => {
-  const domProps = R.omit(['dispatch'], restProps);
-
   return (
     !collapsed && (
-      <div {...domProps} className="vs-category-label">
+      <div {...restProps} className="vs-category-label">
         {children}
       </div>
     )
